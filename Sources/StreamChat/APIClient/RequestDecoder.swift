2d64e781c6ef1cff5201bc646504882d9afec2bb
Christopher Caden <hellocaden@gmail.com>
2021-10-06 14:59:35 -0700

    Update RequestDecoder.swift

--- Sources/StreamChat/APIClient/RequestDecoder.swift
+++ Sources/StreamChat/APIClient/RequestDecoder.swift
@@ -61,7 +61,8 @@ struct DefaultRequestDecoder: RequestDecoder {
             
             log
                 .error(
-                    "API request failed with status code: \(httpResponse.statusCode), code: \(serverError.code) response:\n\(data.debugPrettyPrintedJSON))"
+                    "API request failed with status code: \(httpResponse.statusCode), code: \(serverError.code) response:\n\(data.debugPrettyPrintedJSON))",
+                    "RATE LIMIT REMAINING - - - - - \(httpResponse.allHeaderFields["x-ratelimit-remaining"])"
                 )
             throw ClientError(with: serverError)
         }
